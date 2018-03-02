f78185d7fd4eb60cc233cbc374d431f4278b8967
Gerrit Luimstra <gerrit.luimstra@ziggo.nl>
2018-03-03 00:06:08 +0100

    Add files via upload

---
+++ jenkins.ino
@@ -0,0 +1,43 @@
+// Require all the libs
+#include "lib/helper.h"
+#include "lib/lcd.h"
+#include "lib/engine.h"
+#include "lib/bluetooth.h"
+#include "lib/pinger.h"
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+
+  // Enable the serial port to display log messages
+  Serial.begin(9600);
+    
+  // initialize the LCD
+  lcd.begin();
+  
+  // Turn on the blacklight
+  lcd.backlight();
+  
+  // Show the init message
+  lcd.print("Jenkins - Ready");
+  delay(1000);
+  lcd.clear();
+  lcd.print("Jenkins - ");
+  lcd.print("Engine booting up");
+  delay(2000);
+}
+
+void loop() {
+  lcd.clear();
+  /*
+   * Pinger code
+   */
+   if(object_detected(10)){
+      lcd.print("Detected");
+   } else {
+      lcd.print("Not Detected");
+   }
+
+   delay(1000);
+}
